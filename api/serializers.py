ee9ddc430531fc9ef35f56b19f208550c4f758c4
indalkumaryadav <yadavindal269@gmail.com>
2020-12-17 15:19:14 +0530

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['name', 'roll', 'subject']
