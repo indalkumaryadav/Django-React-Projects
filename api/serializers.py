9e9d309fef67338e37f154dd138e0fdfa4b5d986
indalkumaryadav <yadavindal269@gmail.com>
2021-02-05 08:33:17 +0530

    initial commit

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from rest_framework import serializers
-from .models import Student
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ['name', 'roll', 'subject']
--- api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .serializers import StudentSerializer
-from rest_framework import viewsets
-from .models import Student
-
-
-class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
