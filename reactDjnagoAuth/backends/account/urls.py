39b0e2ce546b40e0c97aab2762ed5e05f23e0879
indalkumaryadav <yadavindal269@gmail.com>
2021-03-18 19:34:54 +0530

    first comit

--- reactDjnagoAuth/backends/account/urls.py
+++ reactDjnagoAuth/backends/account/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from .views import UserAPIView
 
 urlpatterns = [
-    # path('', )
+    path('register/',UserAPIView.as_view())
 ]
--- reactDjnagoAuth/backends/account/views.py
+++ reactDjnagoAuth/backends/account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class UserAPIView(APIView):
+    
+    def post(self,request):
+        user=User.objects.create()
+        user_serializer=UserSerializer(user,many=True)
+        return Response(user_serializer.data)
