7efdd3f775acc288f24a457baa8b2f58ef38409c
indalkumaryadav <yadavindal269@gmail.com>
2021-05-28 18:34:52 +0530

    first commit

--- BlogProject/frontend/src/pages/Home.js
+++ BlogProject/frontend/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const App = () => {
+const Home = () => {
   return (
     <div>
-      <h1>App</h1>
+      <h1>Home</h1>
     </div>
   );
 };
+
+export default Home;
