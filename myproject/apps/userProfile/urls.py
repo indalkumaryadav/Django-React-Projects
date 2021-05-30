1cb9afa9c297d85515607b653fe26cb61303366f
indalkumaryadav <yadavindal269@gmail.com>
2021-05-30 06:35:06 +0530

    first commit

--- myproject/apps/userProfile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from .views import ProfileAPIView
-
-urlpatterns = [
-    
-    path('profile/',ProfileAPIView.as_view() ),
-    path('profile/<int:id>/',ProfileAPIView.as_view() ),
-]
--- myproject/src/server/server.js
+++
@@ -1,2 +0,0 @@
-export const SERVER = "http://127.0.0.1:8000";
-export const API = "http://127.0.0.1:8000/api";
